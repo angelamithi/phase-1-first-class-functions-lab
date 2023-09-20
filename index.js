cb0d8b3e913eb15d846a1cd111b9a83db233ec03
+ <+>
2023-09-20 11:34:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(firstTwodrivers) {
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+firstTwodrivers=drivers.splice(0,2);
+return firstTwodrivers;
+}
+const returnLastTwoDrivers= function(lastTwodrivers) {
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+lastTwodrivers=drivers.splice(2,2);
+return lastTwodrivers;
+}
+const selectingDrivers= [
+    returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier= function(multiplier){
+    let newFare=function(fare){
+    let newfareVariable=fare*multiplier;
+    return newfareVariable;
+    }
+   return newFare;
+   }
+
+const fareDoubler= createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers,returnFirstTwoDrivers){
+    drivers=returnFirstTwoDrivers();
+    return drivers;
+}
